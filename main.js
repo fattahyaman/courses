23a58d89c64369d46215d75efbf31f7cd72b93f2
yaman <maniyaman76@gmail.com>
2022-10-23 01:59:46 +0200

    currency change depending on the location of ip

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // SELECTORS
 const select = document.querySelector(".option");
 const selectElement = document.querySelector("#one");
-const course_title = document.getElementById("course_title");
-const course_date = document.getElementById("course_date");
-
+const courseTitle = document.getElementById("course_title");
+const courseDate = document.getElementById("course_date");
+const coursePrice = document.getElementById("course_price");
 // SELECTORS
 
 // API CALL
@@ -30,12 +30,38 @@ selectElement.addEventListener("change", (event) => {
     .then((response) => response.json())
     .then((data) => {
       // INORDER TO CHANGE TITLE AND DATE
-      course_title.innerText = data.description;
-      course_date.innerHTML = "";
+      courseTitle.innerText = data.description;
+      courseDate.innerHTML = "";
       data.start_dates.forEach((ele) => {
         const p = document.createElement("p");
         p.innerText = ele;
-        course_date.appendChild(p);
+        coursePrice.appendChild(p);
       });
+
+      // TO GET A CURRENT LOCATION
+      var request = new XMLHttpRequest();
+      request.open(
+        "GET",
+        "https://api.ipdata.co/?api-key=73d32d75455fcc03625b5b88f71807c5bafca190fde0d2cc262d2b7f"
+      );
+
+      request.setRequestHeader("Accept", "application/json");
+
+      request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          // INORDER TO CHANGE PRICE
+          course_price.innerHTML = "";
+          const p = document.createElement("p");
+
+          // TO SET IT AS PER THE REGION
+          p.innerText =
+            (JSON.parse(this.responseText).continent_name === "Europe" &&
+              data.prices[1].amount + " " + data.prices[1].currency) ||
+            data.prices[0].amount + " " + data.prices[0].currency;
+
+          course_price.appendChild(p);
+        }
+      };
+      request.send();
     });
 });
